4e358c680c6aed03f5dbd46d896e1346b39e2bdc
Kenneth <wcw84@qq.com>
2023-08-22 21:16:34 +0800

    update controller image

--- controller-image/build_push.sh
+++ controller-image/build_push.sh
@@ -11,6 +11,6 @@ docker tag ${image_name}:latest ${image_url}
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 docker push ${image_url}
-
+echo ${image_url}
 #给deployment加iam role的权限。
 eksctl create iamserviceaccount --cluster=${cluster_name} --region=${AWS_REGION} --name=img2img-sa --attach-policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/AWSContainerSQSQueueExecutionPolicy-discord-sd --approve 
