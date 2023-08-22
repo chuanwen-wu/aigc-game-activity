8f3e22731f8e37f6058f4b10f84558d33f81cc8a
Kenneth <wcw84@qq.com>
2023-08-22 22:01:16 +0800

    update

--- terraform/discord-bot/output.tf
+++ terraform/discord-bot/output.tf
@@ -6,6 +6,10 @@ output "discord_interactions_endpoint_url" {
   value = module.api_gw_lambda.discord_interactions_endpoint_url
 }
 
+output "sqs_queue_url" {
+  value = module.api_gw_lambda.sqs_queue_url
+}
+
 output "project_id" {
   value = local.unique_project
 }
