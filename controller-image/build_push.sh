88a0e0f059b8fa2d9fc4a65819d2171ba997ea30
Kenneth <wcw84@qq.com>
2023-08-22 21:23:53 +0800

    update controller image

--- controller-image/build_push.sh
+++ controller-image/build_push.sh
@@ -2,7 +2,7 @@
 AWS_REGION='ap-northeast-1'
 cluster_name='eks-game-gai'
 image_name=controller-sd
-
+sa_name=img2img-sa
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 aws ecr create-repository --repository-name ${image_name}
 image_url=${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${image_name}:latest
@@ -11,6 +11,9 @@ docker tag ${image_name}:latest ${image_url}
 
 aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
 docker push ${image_url}
-echo ${image_url}
 #给deployment加iam role的权限。
-eksctl create iamserviceaccount --cluster=${cluster_name} --region=${AWS_REGION} --name=img2img-sa --attach-policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/AWSContainerSQSQueueExecutionPolicy-discord-sd --approve 
+eksctl create iamserviceaccount --cluster=${cluster_name} --region=${AWS_REGION} --name=${sa_name} --attach-policy-arn=arn:aws:iam::${ACCOUNT_ID}:policy/AWSContainerSQSQueueExecutionPolicy-discord-sd --approve 
+
+echo "image_url = ${image_url}"
+echo "service_account = ${sa_name}"
+ 
